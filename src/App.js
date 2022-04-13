a09e326e6619f0c463db667f3ad8b7c1cefbb5f6
Laurent Garnier <laurent.garnier.superprof@gmail.com>
2022-04-13 20:44:34 +0200

    Finish app, it's working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './TaskList.js'
+import NewTask from './NewTask.js'
 
 function App() {
-  const tasks = [
-    {id: 0, description: 'do this', done: false},
-    {id: 1, description: 'do that', done: false},
-  ];
+  const [tasks, setTasks] = useState([]);
+  const [nextId, setNextId] = useState(0);
+
+  const createTask = description => {
+    // create task
+    setTasks(tasks => ([...tasks, {id: nextId, description: description, done: false}]));
+    setNextId(nextId + 1);
+  }
+
+  const markTaskDone = id => {
+    const newTaskList = tasks.map(task => {
+      if(task.id === id) {
+        task.done = true;
+      }
+      return task;
+    });
+    setTasks(newTaskList);
+  }
+
+  const deleteTask = id => {
+    setTasks(tasks.filter(task => task.id !== id));
+
+  }
+
   return (
-    <div>
-      <TaskList tasks={tasks}/>
+    <div className="container">
+      <div className="row">
+        <div className='col'>
+          <div className='jumbotron'>
+            <h1 className='display-4'>Todo App</h1>
+          </div>
+          <NewTask createTask={createTask} />
+          <br />
+          <TaskList tasks={tasks} markTaskDone={markTaskDone} deleteTask={deleteTask}/>
+        </div>
+      </div>
     </div>
   );
 }
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Task from './Task.js';
 
-export default({ tasks }) => {
+export default({ tasks, markTaskDone, deleteTask }) => {
     
     return (
         <ul className="list-group">
             {tasks.map(task => (
                 <li key={task.id} className="list-group-item">
-                    <Task task={task} />
+                    <Task task={task} markTaskDone={markTaskDone} deleteTask={deleteTask}/>
                 </li>
             ))}
         </ul>
